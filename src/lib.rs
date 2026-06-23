9c5f7de3a9780d4b32041656dcb932c48bb5f135
Miner3D <miner3dgamer@gmail.com>
2026-06-23 06:12:31 +0200

    Moved code into other crates

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,14 @@
 // Const
 #![allow(incomplete_features)]
 #![feature(const_trait_impl)]
-#![feature(const_cmp)]
-#![feature(const_eval_select)]
 #![feature(generic_const_exprs)]
 // #![feature(const_ops)]
 // #![feature(const_destruct)]
 // Accessing core
-#![feature(core_intrinsics)]
-#![feature(str_internals)]
+// #![feature(core_intrinsics)]
 // Misc
-#![feature(decl_macro)]
 // #![feature(rustc_allow_const_fn_unstable)]
 #![feature(rustc_attrs)]
-
-#[cfg(feature = "std")]
-/// Text related stuff
-pub mod text;
 
 // // /// Stuff that should exist by default yet doesn't; use `mirl::extensions::*;` to import all of 'em
 // // pub mod extensions;
@@ -42,15 +34,12 @@ pub mod math;
 /// Mostly just traits
 pub mod prelude;
 
-/// Stuff that hasn't been sorted yet
-#[cfg(feature = "std")]
-pub mod misc;
+// /// Stuff that hasn't been sorted yet
+// #[cfg(feature = "std")]
+// pub mod misc;
 
 //
 // pub mod constants;
 
 /// Re-export of all used dependencies
 pub mod dependencies;
-
-/// Macro definitions
-pub mod macros;
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,13 +1,2 @@
-/// Represents digital keys using `KeyCodes` of which there should be plenty enough to pretty all libraries that use their own `KeyCodes`
-pub mod keycodes;
-// /// Mouse related items
-// pub mod mouse;
-
-pub use keycodes::KeyCode;
-
-/// Keyboard manager, check/set if and when keys are pressed
-pub mod keyboard_manager;
-pub use keyboard_manager::KeyboardState;
-
 /// Fs related stuff
 pub mod file_system;
